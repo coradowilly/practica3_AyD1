c80a3f76a3cea410e8a341753493f66236cb4864
coradowilly <coradowilly@gmail.com>
2020-10-12 17:48:45 -0600

    componentes

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component'
+import { CatalogoComponent } from './components/catalogo/catalogo.component';
+import { PagoComponent } from './components/pago/pago.component';
+import { HistorialComponent } from './components/historial/historial.component';
+import { InventarioComponent } from './components/inventario/inventario.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CatalogoComponent,
+    PagoComponent,
+    HistorialComponent,
+    InventarioComponent,
+    PerfilComponent
   ],
   imports: [
     BrowserModule,
