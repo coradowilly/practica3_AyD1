cc2e1067b2bd106bbe08e3bcad2825e0b4d4dd57
NeDGUndPanzer <tesco.herrera.edu@gmail.com>
2020-10-15 15:17:45 -0600

    TDD Catalogo - Se realizan metodos de componente

--- FrontEnd/src/app/components/catalogo/catalogo.component.ts
+++ FrontEnd/src/app/components/catalogo/catalogo.component.ts
@@ -26,12 +26,34 @@ export class CatalogoComponent implements OnInit
 
   public showCatalog():any
   {
-    
+      this.cards = 
+      [
+        { id:1, name: "card1", image: "image1", chargeRate: "chargeRate1", Active:"Ative1", availability:"availability1" },
+        { id:2, name: "card2", image: "image2", chargeRate: "chargeRate2", Active:"Ative2", availability:"availability2" },
+        { id:3, name: "card3", image: "image3", chargeRate: "chargeRate3", Active:"Ative3", availability:"availability3" }
+      ];
+    /*
+    this.catalogo.getProductos().subscribe(
+      (res) =>
+      {
+        console.log('Respuesta Node',res);
+        this.cards = res;
+      },
+      (error) =>
+      {
+        console.log(error)
+      }
+    );*/   
   }
 
   public addCarrito(item:any)
   {
-      
+      let aux:carrito = 
+      { 
+        id:item.id, name: item.name, image: item.image, chargeRate: item.chargeRate, Active:item.Active, availability:item.availability 
+      };
+      this.carrito.push(aux);
+      console.log(aux);
   }
 
 }
