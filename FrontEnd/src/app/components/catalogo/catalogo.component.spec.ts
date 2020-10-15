d701a27e7adc513b4da445fc27f9d43dea400f41
NeDGUndPanzer <tesco.herrera.edu@gmail.com>
2020-10-15 15:15:40 -0600

    TDD Catalogo - Se realizan pruebas para componente

--- FrontEnd/src/app/components/catalogo/catalogo.component.spec.ts
+++ FrontEnd/src/app/components/catalogo/catalogo.component.spec.ts
@@ -1,13 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {RouterTestingModule} from '@angular/router/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import {carrito} from '../../models/carrito';
 import { CatalogoComponent } from './catalogo.component';
 
-describe('CatalogoComponent', () => {
+fdescribe('CatalogoComponent', () => {
   let component: CatalogoComponent;
   let fixture: ComponentFixture<CatalogoComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      providers: [],
       declarations: [ CatalogoComponent ]
     })
     .compileComponents();
@@ -22,4 +26,31 @@ describe('CatalogoComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+  describe("Given: Dados los datos obtenidos de la API", function(){  
+    describe("When: La pagina carga", function(){
+      describe("Then: La variable cards obtiene valores de la API", function(){
+          it('Muestra ', () => {
+              component.showCatalog();
+              expect(component.cards).toBeDefined();
+          });
+      });
+    });
+  });
+
+  describe("Given: El un producto se ha seleccioado", function(){  
+    let CarritoSize:number;
+    describe("When: El cliente selecciona un producto del catalogo", function(){
+      describe("Then: El arreglo de productos se incrementa en uno", function(){
+          it('Muestra ', () => {
+              CarritoSize = component.carrito.length;
+              let aux = { id:1024, name: "prueba", image: "prueba", chargeRate: "prueba", Active:"prueba", availability:"prueba" };
+              component.addCarrito(aux);
+              expect(component.carrito.length).toEqual(CarritoSize + 1);
+          });
+      });
+    });
+  });
+
 });
--- FrontEnd/src/app/components/catalogo/catalogo.component.ts
+++ FrontEnd/src/app/components/catalogo/catalogo.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import {carrito} from '../../models/carrito';
 
 @Component({
   selector: 'app-catalogo',
   templateUrl: './catalogo.component.html',
   styleUrls: ['./catalogo.component.css']
 })
-export class CatalogoComponent implements OnInit {
+export class CatalogoComponent implements OnInit 
+{
 
-  constructor() { }
+  public cards:any;
+  public carrito: Array<carrito>; 
 
-  ngOnInit(): void {
+  constructor() 
+  { 
+      /** Codigo constructor -- */
+      this.carrito = new Array<carrito>();
+      this.showCatalog();
+  }
+
+  ngOnInit(): void 
+  {
+      /** Codigo -- */
+  }
+
+  public showCatalog():any
+  {
+    
+  }
+
+  public addCarrito(item:any)
+  {
+      
   }
 
 }
