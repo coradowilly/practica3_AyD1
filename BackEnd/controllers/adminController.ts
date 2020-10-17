81c832def05b7ab474824466424384457321d1a0
Eliel <elielbarrios97@gmail.com>
2020-10-17 02:34:55 -0600

    Se agregaron rutas y controladores para administracion

---
+++ BackEnd/controllers/adminController.ts
@@ -0,0 +1,14 @@
+import {Response, Request} from 'express';
+import db from '../conf/database';
+
+class AdminController
+{
+
+    /*async getAvailableGiftcards(req : Request, res : Response) : Promise<void> //obtener todas las Categorias
+    {
+        res.json({'code':200});
+    }*/
+    
+}
+
+export const adminController = new AdminController();
---
+++ BackEnd/routes/adminRoutes.ts
@@ -0,0 +1,21 @@
+import {Router} from 'express';
+import {adminController} from '../controllers/adminController'
+
+
+class AdminRoutes
+{
+    public router : Router;
+
+    constructor()
+    {
+        this.router = Router();
+        this.config();
+    }
+
+    config() : void
+    {
+        //this.router.get('/admin',adminRoutes);
+    }
+}
+const adminRoutes = new AdminRoutes();
+export default adminRoutes.router;
