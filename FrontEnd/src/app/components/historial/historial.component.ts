75ca3a8d55e038d792392f423f85673a055c283e
NeDGUndPanzer <tesco.herrera.edu@gmail.com>
2020-10-16 23:37:36 -0600

    TDD Historial - se agregan pruebas del componentes

--- FrontEnd/src/app/components/historial/historial.component.ts
+++ FrontEnd/src/app/components/historial/historial.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router"
+import {HistorialService} from '../../services/historial.service';
 
 @Component({
   selector: 'app-historial',
   templateUrl: './historial.component.html',
   styleUrls: ['./historial.component.css']
 })
-export class HistorialComponent implements OnInit {
+export class HistorialComponent implements OnInit 
+{
 
-  constructor() { }
+  public historial:any;
 
-  ngOnInit(): void {
+  constructor(private historialService:HistorialService, private router: Router) 
+  { 
+      /** Codigo de constructor -- */
+  }
+
+  ngOnInit(): void 
+  {
+      /** Codigo de ngOnInit -- */
+  }
+
+  public showHistorial() 
+  { 
+      /** Codigo de showHistorial -- */
+      
+      this.historialService.getHistorial().subscribe(
+        (res) =>
+        {
+          console.log(res);
+          this.historial = res;
+        },
+        (error) =>
+        {
+          console.log(error)
+        }
+      ); 
   }
 
 }
---
+++ FrontEnd/src/app/services/historial.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HistorialService 
+{
+
+  API_URI = 'http://52.15.119.221:3000/api/ruta';
+
+  constructor(private http: HttpClient) 
+  { 
+      /** constructor */
+  }
+
+  getHistorial()
+  {
+    return this.http.get(`${this.API_URI}`);
+  }
+  
+}
