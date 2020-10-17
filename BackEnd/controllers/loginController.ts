3f07bb2319ed4b8edc78f0638b4e0fd23bc8d860
Saul Guzman <guzmanmsaul@gmail.com>
2020-10-17 11:14:35 -0600

    Se arreglo controlador de Login

--- BackEnd/controllers/loginController.ts
+++ BackEnd/controllers/loginController.ts
@@ -8,8 +8,15 @@ class loginController
     {
         const { correo, contrasena} = req.body;
        // const query = await db.query('SELECT * FROM usuario WHERE correo=? and contrasena=?',[correo,contrasena]);
-        const query = [{correo:"prueba@gmail.com", contrasena: "12345"}];         
-        if(query.length == 1){
+        const query = [{correo:"prueba35@gmail.com", contrasena: "35465"},{correo:"prueba@gmail.com", contrasena: "12345"},{correo:"prueba22@gmail.com", contrasena: "684"}];         
+        var acceso=0;
+        for(let item of query){
+            if(item.correo == correo && item.contrasena == contrasena){
+                acceso = 1;
+            }
+        }
+
+        if(acceso == 1){
             console.log('Acceso correcto');
             res.status(200).json({estado: 1});
         }else{
