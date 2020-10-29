56f53649f476ffc88edf6d504d57b0eb92ef7844
coradowilly <coradowilly@gmail.com>
2020-10-29 11:16:36 -0600

    Perfil funcional

--- FrontEnd/src/app/components/perfil/perfil.component.ts
+++ FrontEnd/src/app/components/perfil/perfil.component.ts
@@ -1,5 +1,6 @@
 import { Component, HostBinding, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { PerfilService } from 'src/app/services/perfil.service';
 
 @Component({
   selector: 'app-perfil',
@@ -11,30 +12,41 @@ export class PerfilComponent implements OnInit {
   @HostBinding('class') classes = 'row';
   
   newUser:any = {
-    nombre:"Alvin",
-    apellido: "Espino",
-    email:"alvine@gmail.com",
-    password:"54321",
-    celular:"88877700",
-    nit:"123sa5-5",
-    direccion:"usac ingenieria"
+    id:0,
+    username:"",
+    email: "",
+    password:"",
+    firstname:"",
+    lastname:"",
+    dpi:"",
+    age:0
   }
   
-  constructor(private router: Router) { }
+  constructor(private router: Router, private servicio:PerfilService) { }
 
   ngOnInit(): void {
+    this.newUser = JSON.parse(sessionStorage.getItem("user"));
   }
 
   actualizar(){
-    
+    this.servicio.update(this.newUser)
+    .subscribe(
+      res=>{
+        this.isSuccess("Actualizado con exito");
+        sessionStorage.setItem("user",JSON.stringify(this.newUser));
+      },
+      err=>{
+        this.isError("No se pudo actualizar");
+      }
+    )
   }
 
   isError(str:string){
-
+    alert(str)
   }
 
   isSuccess(str:string){
-
+    alert(str)
   }
 
 }
